a871a315704abd48c018c300a696bc57a3de5121
marojeon-snuailab <maro.jeon@snuailab.ai>
2022-08-14 20:25:13 +0900

    refactor: main def to utils

---
+++ common/utils.py
@@ -0,0 +1,331 @@
+import sys
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst
+import pyds
+import ctypes
+import numpy as np
+
+PGIE_CLASS_ID_PERSON = 0
+PGIE_CLASS_ID_OBJECT = 1
+PGIE_CLASS_ID_FIRE = 2
+past_tracking_meta=[0]
+
+def layer_finder(output_layer_info, name):
+    """ Return the layer contained in output_layer_info which corresponds
+        to the given name.
+    """
+    for layer in output_layer_info:
+        # dataType == 0 <=> dataType == FLOAT
+        if layer.dataType == 0 and layer.layerName == name:
+            return layer
+    return None
+
+def make_elm_or_print_err(factoryname, name, printedname, detail=""):
+    """ Creates an element with Gst Element Factory make.
+        Return the element  if successfully created, otherwise print
+        to stderr and return None.
+    """
+    print("Creating", printedname)
+    elm = Gst.ElementFactory.make(factoryname, name)
+    if not elm:
+        sys.stderr.write("Unable to create " + printedname + " \n")
+        if detail:
+            sys.stderr.write(detail)
+    return elm
+
+def osd_sink_pad_buffer_probe(pad,info,u_data):
+    frame_number=0
+    obj_counter = {
+        PGIE_CLASS_ID_PERSON:0,
+        PGIE_CLASS_ID_OBJECT:0,
+        PGIE_CLASS_ID_FIRE:0
+    }
+    num_rects=0
+
+    gst_buffer = info.get_buffer()
+    if not gst_buffer:
+        print("Unable to get GstBuffer ")
+        return
+
+    batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+
+    l_frame = batch_meta.frame_meta_list
+    while l_frame is not None:
+        try:
+            frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
+        except StopIteration:
+            break
+
+        frame_number=frame_meta.frame_num
+        num_rects = frame_meta.num_obj_meta
+
+        l_obj=frame_meta.obj_meta_list
+        while l_obj is not None:
+            try:
+                obj_meta=pyds.NvDsObjectMeta.cast(l_obj.data)
+            except StopIteration:
+                break
+            print("obj_meta.class_id", obj_meta.class_id)
+            obj_counter[obj_meta.class_id] += 1
+            obj_meta.rect_params.border_color.set(0.0, 0.0, 1.0, 0.0)
+
+            try: 
+                l_obj=l_obj.next
+            except StopIteration:
+                break
+
+        display_meta=pyds.nvds_acquire_display_meta_from_pool(batch_meta)
+        display_meta.num_labels = 1
+        py_nvosd_text_params = display_meta.text_params[0]
+
+        py_nvosd_text_params.display_text = "Frame Number={} Number of Objects={} Fire_count={} Person_count={}".format(frame_number, num_rects, obj_counter[PGIE_CLASS_ID_FIRE], obj_counter[PGIE_CLASS_ID_PERSON])
+
+        py_nvosd_text_params.x_offset = 10
+        py_nvosd_text_params.y_offset = 12
+
+        py_nvosd_text_params.font_params.font_name = "Serif"
+        py_nvosd_text_params.font_params.font_size = 10
+        py_nvosd_text_params.font_params.font_color.set(1.0, 1.0, 1.0, 1.0)
+
+        py_nvosd_text_params.set_bg_clr = 1
+        py_nvosd_text_params.text_bg_clr.set(0.0, 0.0, 0.0, 1.0)
+        print(pyds.get_string(py_nvosd_text_params.display_text))
+        pyds.nvds_add_display_meta_to_frame(frame_meta, display_meta)
+
+        try:
+            l_frame=l_frame.next
+        except StopIteration:
+            break
+
+    #past traking meta data
+    if(past_tracking_meta[0]==1):
+        l_user=batch_meta.batch_user_meta_list
+        while l_user is not None:
+            try:
+                user_meta=pyds.NvDsUserMeta.cast(l_user.data)
+            except StopIteration:
+                break
+            if(user_meta and user_meta.base_meta.meta_type==pyds.NvDsMetaType.NVDS_TRACKER_PAST_FRAME_META):
+                try:
+                    pPastFrameObjBatch = pyds.NvDsPastFrameObjBatch.cast(user_meta.user_meta_data)
+                except StopIteration:
+                    break
+                for trackobj in pyds.NvDsPastFrameObjBatch.list(pPastFrameObjBatch):
+                    print("streamId=",trackobj.streamID)
+                    print("surfaceStreamID=",trackobj.surfaceStreamID)
+                    for pastframeobj in pyds.NvDsPastFrameObjStream.list(trackobj):
+                        print("numobj=",pastframeobj.numObj)
+                        print("uniqueId=",pastframeobj.uniqueId)
+                        print("classId=",pastframeobj.classId)
+                        print("objLabel=",pastframeobj.objLabel)
+                        for objlist in pyds.NvDsPastFrameObjList.list(pastframeobj):
+                            print('frameNum:', objlist.frameNum)
+                            print('tBbox.left:', objlist.tBbox.left)
+                            print('tBbox.width:', objlist.tBbox.width)
+                            print('tBbox.top:', objlist.tBbox.top)
+                            print('tBbox.right:', objlist.tBbox.height)
+                            print('confidence:', objlist.confidence)
+                            print('age:', objlist.age)
+            try:
+                l_user=l_user.next
+            except StopIteration:
+                break
+
+    return Gst.PadProbeReturn.OK	
+
+
+def cb_newpad(decodebin, decoder_src_pad, data):
+    print("In cb_newpad\n")
+    caps=decoder_src_pad.get_current_caps()
+    gststruct=caps.get_structure(0)
+    gstname=gststruct.get_name()
+    source_bin=data
+    features=caps.get_features(0)
+
+    # Need to check if the pad created by the decodebin is for video and not
+    # audio.
+    print("gstname=",gstname)
+    if(gstname.find("video")!=-1):
+        # Link the decodebin pad only if decodebin has picked nvidia
+        # decoder plugin nvdec_*. We do this by checking if the pad caps contain
+        # NVMM memory features.
+        print("features=",features)
+        if features.contains("memory:NVMM"):
+            # Get the source bin ghost pad
+            bin_ghost_pad=source_bin.get_static_pad("src")
+            if not bin_ghost_pad.set_target(decoder_src_pad):
+                sys.stderr.write("Failed to link decoder src pad to source bin ghost pad\n")
+        else:
+            sys.stderr.write(" Error: Decodebin did not pick nvidia decoder plugin.\n")
+
+def decodebin_child_added(child_proxy,Object,name,user_data):
+    print("Decodebin child added:", name, "\n")
+    if(name.find("decodebin") != -1):
+        Object.connect("child-added",decodebin_child_added,user_data)
+
+def create_source_bin(index,uri):
+    print("Creating source bin")
+
+    # Create a source GstBin to abstract this bin's content from the rest of the
+    # pipeline
+    bin_name="source-bin-%02d" %index
+    print(bin_name)
+    nbin=Gst.Bin.new(bin_name)
+    if not nbin:
+        sys.stderr.write(" Unable to create source bin \n")
+
+    # Source element for reading from the uri.
+    # We will use decodebin and let it figure out the container format of the
+    # stream and the codec and plug the appropriate demux and decode plugins.
+    uri_decode_bin=Gst.ElementFactory.make("uridecodebin", "uri-decode-bin")
+    if not uri_decode_bin:
+        sys.stderr.write(" Unable to create uri decode bin \n")
+    # We set the input uri to the source element
+    uri_decode_bin.set_property("uri",uri)
+    # Connect to the "pad-added" signal of the decodebin which generates a
+    # callback once a new pad for raw data has beed created by the decodebin
+    uri_decode_bin.connect("pad-added", cb_newpad, nbin)
+    uri_decode_bin.connect("child-added", decodebin_child_added, nbin)
+
+    # We need to create a ghost pad for the source bin which will act as a proxy
+    # for the video decoder src pad. The ghost pad will not have a target right
+    # now. Once the decode bin creates the video decoder and generates the
+    # cb_newpad callback, we will set the ghost pad target to the video decoder
+    # src pad.
+    Gst.Bin.add(nbin,uri_decode_bin)
+    bin_pad=nbin.add_pad(Gst.GhostPad.new_no_target("src",Gst.PadDirection.SRC))
+    if not bin_pad:
+        sys.stderr.write(" Failed to add ghost pad in source bin \n")
+        return None
+    return nbin
+
+def pgie_src_pad_buffer_probe(pad, info, u_data):
+    gst_buffer = info.get_buffer()
+    if not gst_buffer:
+        print("Unable to get GstBuffer ")
+        return
+
+    batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+
+    l_frame = batch_meta.frame_meta_list
+    while l_frame is not None:
+        try:
+            frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
+        except StopIteration:
+            break
+
+        l_user = frame_meta.frame_user_meta_list
+
+        while l_user is not None:
+            try:
+                user_meta = pyds.NvDsUserMeta.cast(l_user.data)
+            except StopIteration:
+                break
+
+            tensor_meta = pyds.NvDsInferTensorMeta.cast(user_meta.user_meta_data)
+
+            layers_info = []
+
+            for i in range(tensor_meta.num_output_layers):
+                layer = pyds.get_nvds_LayerInfo(tensor_meta, i)
+                layers_info.append(layer)
+
+            print("layers_info", layers_info)
+            output_layer1 = layer_finder(layers_info, "output")
+            print("output_layer1", output_layer1)
+            output_layer_ptr1 = ctypes.cast(pyds.get_ptr(output_layer1.buffer), ctypes.POINTER(ctypes.c_float))
+            print("output_layer1.dims.d[0]", output_layer1.dims.d[0],  output_layer1.dims.d)
+            output_layer_v1 = np.ctypeslib.as_array(output_layer_ptr1, shape=(1, output_layer1.dims.d[0], output_layer1.dims.d[1]))
+            print("output_layer_v1", output_layer_v1.shape)
+            # frame_object_list = nvds_infer_parse_custom_tf_ssd(
+            #     layers_info, detection_params, box_size_param, nms_param
+            # )
+
+            try:
+                l_user = l_user.next
+
+            except StopIteration:
+                break
+
+            # for frame_object in frame_object_list:
+            #     add_obj_meta_to_frame(frame_object, batch_meta, frame_meta, label_names)
+
+        try:
+            l_frame = l_frame.next
+
+        except StopIteration:
+            break
+
+    return Gst.PadProbeReturn.OK
+
+def sgie_src_pad_buffer_probe(pad, info, u_data):
+
+    gst_buffer = info.get_buffer()
+    if not gst_buffer:
+        print("Unable to get GstBuffer ")
+        return
+
+    batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+
+    l_frame = batch_meta.frame_meta_list
+    while l_frame is not None:
+        try:
+            frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
+        except StopIteration:
+            break
+
+        l_obj = frame_meta.obj_meta_list
+        while l_obj is not None:
+            try:
+                obj_meta = pyds.NvDsObjectMeta.cast(l_obj.data)
+            except StopIteration:
+                break
+
+            l_user = obj_meta.obj_user_meta_list
+            while l_user is not None:
+                try:
+                    user_meta = pyds.NvDsUserMeta.cast(l_user.data)
+                except StopIteration:
+                    break
+
+                if (
+                    user_meta.base_meta.meta_type != 
+                    pyds.NvDsMetaType.NVDSINFER_TENSOR_OUTPUT_META
+                ):
+                    continue
+
+                tensor_meta = pyds.NvDsInferTensorMeta.cast(user_meta.user_meta_data)
+
+                layers_info = []
+                for i in range(tensor_meta.num_output_layers):
+                        layer = pyds.get_nvds_LayerInfo(tensor_meta, i)
+                        layers_info.append(layer)
+
+                output_layer1 = layer_finder(layers_info, "prediction1")
+                output_layer_ptr1 = ctypes.cast(pyds.get_ptr(output_layer1.buffer), ctypes.POINTER(ctypes.c_float))
+                output_layer_v1 = np.ctypeslib.as_array(output_layer_ptr1, shape=(1, output_layer1.dims.d[0]))
+
+                output_layer2 = layer_finder(layers_info, "prediction2")
+                output_layer_ptr2 = ctypes.cast(pyds.get_ptr(output_layer2.buffer), ctypes.POINTER(ctypes.c_float))
+                output_layer_v2 = np.ctypeslib.as_array(output_layer_ptr2, shape=(1, output_layer2.dims.d[0]))
+
+                # print(output_layer_v1)
+                # print(output_layer_v2)
+
+                try:
+                    l_user = l_user.next
+                except StopIteration:
+                    break
+
+            try:
+                l_obj = l_obj.next
+            except StopIteration:
+                break
+
+        try:
+            l_frame = l_frame.next
+        except StopIteration:
+            break
+
+    return Gst.PadProbeReturn.OK
