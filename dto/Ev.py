96029c73d92007308009b23ac3519720205029b9
wjsakfh <wjsakfh@gmail.com>
2022-10-02 22:18:20 +0900

    feat: 이벤트 인포에 count 추가

--- dto/Ev.py
+++ dto/Ev.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Any
 from dataclasses import dataclass
 from dto import PgieObj
 
@@ -15,11 +15,13 @@ class EventConfig:
 
 class Event:
     def __init__(self, event_config: EventConfig):
-        self.stream_id = event_config.source_id
+        self.source_id = event_config.source_id
         self.enable = event_config.enable
-        self.event_name = event_config.name
+        self.name = event_config.name
         self.label = event_config.label  # pgie class label
         self.event_timeout = event_config.timeout
         self.roi = event_config.roi
 
         self.obj_list: List[PgieObj] = []  # 이벤트에 등록될 object의 리스트를 초기화
+
+        self.info: Dict[str, Any] = {"count_in": 0, "count_out": 0, "status": str()}
